f5a95c370bb59fa209c3b1cdf18aa8be0bff06bc
Luiz Gustavo <luizgguimaraes@hotmail.com>
2018-11-22 15:34:47 -0500

    C

--- TP2/EstudosDeCaso.c
+++ TP2/EstudosDeCaso.c
@@ -9,34 +9,38 @@
 #include <time.h>
 #include <string.h>
 #define BUFF 20
+#define TAILLE 4
 
 void genererGrille()
 {
 	char grille[4][4];
 	int indexL;
 	int indexC;
-	int taille = 4;
+	
 
-	for(indexL = 0; indexL < taille; indexL++)
+	for(indexL = 0; indexL < TAILLE; indexL++)
 	{
-		for(indexC = 0; indexC < taille; indexC++)
+		for(indexC = 0; indexC < TAILLE; indexC++)
 		{
 			grille[indexL][indexC] = rand() % ('Z' - 'A' + 1) + 'A';
 		}
 	}
 
-	for(indexL = 0; indexL < taille; indexL++)
+	for(indexL = 0; indexL < TAILLE; indexL++)
 	{
-		for(indexC = 0; indexC < taille; indexC++)
+		for(indexC = 0; indexC < TAILLE; indexC++)
 		{
 			printf(" %c ",grille[indexL][indexC]);
 		}
 		printf("\n");
 	}
+	
 }
 
-int lireFichier(char *filename)
+int lireFichier(char *filename, char motForme[])
 {
+	int a = 0;
+	int b = 0;
     FILE *file = fopen(filename, "r");
     int nblines = 0;
   
@@ -46,28 +50,31 @@ int lireFichier(char *filename)
 	exit(1);
       }
 
-    
     char mot[BUFF];
     while (fgets(mot, BUFF, file) != NULL) 
     {
 	int l = strlen(mot);
 	mot[l - 1] = '\0';
-	printf("%s, ", mot);
-    }
-    
+	
+	a = strlen(mot);
+	if(a >= b)
+	{
+		b = a;
+	}
+
+	if(!strcmp(mot, motForme)) return 1;
+	}
+    printf(" *** %d ****",b);
     fclose(file);
-    return nblines;
+     return nblines;
 }
 
 int main()
 {
 	srand(time(NULL));
-	
-
-
-	lireFichier("francais.txt");
-
-
-
-
+	char motForme[18];
+	genererGrille();
+	gets(motForme);
+	if(lireFichier("francais.txt",motForme) == 1) printf(" Encontrou!\n");
+	else printf(" NAO Encontrou!\n");
 }
